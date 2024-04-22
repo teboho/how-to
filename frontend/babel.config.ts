b57d0add980e8b735a44a60d801249fa5b4c32a0
teboho <tebohomakhalemele@outlook.com>
2024-04-22 16:42:41 +0200

    setup for testing next js components

--- frontend/babel.config.ts
+++ frontend/babel.config.ts
@@ -1,15 +1,13 @@
 module.exports = {
     presets: [
         [
-        '@babel/preset-react',
-        '@babel/preset-env',
-        {
-            targets: {
-            node: 'current',
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
             },
-        },
         ],
-        '@babel/preset-typescript',
+        '@babel/preset-typescript'
     ],
 };
-//
