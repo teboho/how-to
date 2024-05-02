1d666caf93f64ef295417022163dbfeba92283be
teboho <tebohomakhalemele@outlook.com>
2024-05-02 16:59:10 +0200

    executorygategory bridge support

---
+++ backend/src/Boxfusion.HowTo.Application/Services/ExecutorCategoryAppService/ExecutorCategoryAppService.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services;
+using Abp.Domain.Repositories;
+using Boxfusion.HowTo.Services.TaskAppService.Dtos;
+using System;
+
+namespace Boxfusion.HowTo.Services.TaskAppService
+{
+    public class ExecutorCategoryAppService : AsyncCrudAppService<Domain.ExecutorCategory, ExecutorCategoryDto, Guid>, IExecutorCategoryAppService
+    {
+        IRepository<Domain.ExecutorCategory, Guid> _repository;
+        public ExecutorCategoryAppService(IRepository<Domain.ExecutorCategory, Guid> repository) : base(repository)
+        {
+            _repository = repository;
+        }
+    }
+}
